4a80155b7d7d3e555abe25c2f8d48d1e281ec6c7
Hosin <hosin_asfour@dell.com>
2020-10-13 17:21:58 +0000

    fixed first tag

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "azurerm_virtual_machine" "catapp" {
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
 
-  tags = {
+  tags {
     Department tag  =  devops
   }
 
